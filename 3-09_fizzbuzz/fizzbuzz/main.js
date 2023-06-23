04e8935acdbc3e2ac9369762b105aab79934beec
yuuuuuuuuuuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2023-06-23 16:19:15 +0900

    0623-02

--- 3-09_fizzbuzz/fizzbuzz/main.js
+++ 3-09_fizzbuzz/fizzbuzz/main.js
@@ -1,5 +1,14 @@
 console.log('test')
 
+/*
+-引数は num とし number型
+-3で割り切れる → Fizz!
+-5で割り切れる → Buzz!
+-3,5の両方で割り切れる → FizzBuzz!
+-それ以外は 入力値をそのまま返す
+
+'*/
+
 function fizzbuzz(num) {
     if (num % 3 === 0 && num % 5 === 0) {
         return 'fizzbuzz';
